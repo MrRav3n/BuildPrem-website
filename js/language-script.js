8daff050123026218bb38b3a9f84e6ab9cf0d153
MrRav3n <dawidmrraven@gmail.com>
2020-10-14 23:50:32 +0200

    everything finished

--- js/language-script.js
+++ js/language-script.js
@@ -1,6 +1,15 @@
 (() => {
     const lang = navigator.language
-    if(lang !== 'pl-PL') {
+    let location = window.location.href.split('/');
+    location = location[location.length-1];
+    let dontNavigate = sessionStorage.getItem('dontNavigate')
+    if(lang !== 'pl-PL' && location !== 'index.en.html' && !dontNavigate) {
+        localStorage.setItem('dontNavigate', 'true');
+        console.log('Navigating to ENG version');
         window.location.replace('./index.en.html');
     }
 })();
+
+dontNavigate = () => {
+    sessionStorage.setItem('dontNavigate', 'true');
+}
