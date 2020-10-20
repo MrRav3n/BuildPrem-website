63c344e0b29c5de4efb48dc984535fec4a6cf67c
MrRav3n <dawidmrraven@gmail.com>
2020-10-20 20:28:52 +0200

    zmiany

--- js/scripts.js
+++ js/scripts.js
@@ -45,8 +45,12 @@
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
+            $("#logoNav1").addClass("d-none");
+            $("#logoNav2").addClass("d-flex");
         } else {
             $("#mainNav").removeClass("navbar-shrink");
+            $("#logoNav1").removeClass("d-none");
+            $("#logoNav2").removeClass("d-flex");
         }
     };
     // Collapse now if page is not at top
